acfc768bc7106c2aee1e178e62339c54045207df
Colette Montminy <31236022+crmontminy@users.noreply.github.com>
2019-06-29 01:22:25 +0300

    initial upload

---
+++ test.py
@@ -0,0 +1,61 @@
+import discord
+from discord.ext import commands
+from datetime import datetime
+import io
+import aiohttp
+#owo
+description = "a disc bot to handle pins a lil better"
+bot = commands.Bot(command_prefix='!', description=description)
+
+defaultChannel = 0
+#defaultChannel = bot.get_channel(593894678688956531)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+#Channel set function
+@bot.command()
+async def setchannel(ctx):
+    global defaultChannel
+    defaultChannel = ctx.message.channel.id
+    await ctx.send("{} has been set as the default pin channel, silly! ow< ~☆".format(defaultChannel))
+
+#Pin mover
+@bot.command()
+async def migrate(ctx):
+    migration = ctx.message.channel 
+    pins = await migration.pins()
+    if len(pins) != 0:
+        for x in pins:
+            
+            embed = discord.Embed(title = "")
+            embed.set_author(name = x.author.display_name, icon_url = x.author.avatar_url)
+            
+            if len(x.attachments) != 0:
+                for y in x.attachments:
+                    async with aiohttp.ClientSession() as session:
+                        async with session.get(y.url) as resp:
+                            if resp.status != 200:
+                                return await ctx.send('File not found, silly! >w<') #how do i make a sad owo face
+                            data = io.BytesIO(await resp.read())
+                            await bot.get_channel(defaultChannel).send(file=discord.File(data, y.filename))
+            print(x.content)
+            if x.content != embed.Empty:
+                embed.add_field(name = x.content, value = x.created_at.strftime("%D @ %T %p") + " [jump](" + x.jump_url + ")")
+            else:
+                embed.add_field(name = "", value = x.created_at.strftime("%D @ %T %p") + " [jump](" + x.jump_url + ")")
+            await bot.get_channel(defaultChannel).send(embed = embed)
+        
+        await ctx.send("Messages have been migrated, silly! uwu")
+    else: await ctx.send("There aren't any pins here, silly! òwó")
+
+#literally just to make sure it works
+@bot.command()
+async def test(ctx):
+    await ctx.send("yo i work, silly! OwO")
+
+bot.run('TOKEN')
