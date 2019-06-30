5dc8633d060c13b409fcfb10be4a58f8d69090f6
seemspromising <43363614+seemspromising@users.noreply.github.com>
2019-06-30 16:14:33 -0400

    Update test.py
    added some embed handling, random pin generator, and other minor improvements

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from datetime import datetime
 import io
 import aiohttp
+import random
 #owo
 description = "a disc bot to handle pins a lil better"
 bot = commands.Bot(command_prefix='!', description=description, case_insensitive=True)
@@ -21,7 +22,7 @@ async def on_ready():
 async def setchannel(ctx):
     global defaultChannel
     defaultChannel = ctx.message.channel.id
-    await ctx.send("#{} has been set as the default pin channel, silly! ow< ~☆".format(ctx.message.channel))
+    await ctx.send("{} has been set as the default pin channel, silly! ow< ~☆".format(ctx.message.channel.mention))
 # L M A O FUCK OFF WITH THAT LOWERCASE BITCH O H  M Y  G O D BWETTER TWANK U UWU
 
 #Pin mover owo
@@ -30,6 +31,7 @@ async def migrate(ctx):
     migration = ctx.message.channel 
     pins = await migration.pins()
     if len(pins) != 0:
+        #async with bot.get_channel(defaultChannel).typing(): (issue: wonky... so i didnt implement :'D)
         for x in reversed(pins):
             embed = discord.Embed(title = "")
             embed.set_author(name = x.author.display_name, icon_url = x.author.avatar_url)
@@ -39,22 +41,32 @@ async def migrate(ctx):
                     embed.add_field(name = x.clean_content[:253] + "...", value = x.created_at.strftime("%D @ %T %p") + " - this message was hecka long, silly! ☆W☆ [jump to see the rest](" + x.jump_url + ")")
                 else: 
                     embed.add_field(name = x.clean_content, value = x.created_at.strftime("%D @ %T %p") + " - [jump](" + x.jump_url + ")")
-                
-                if len(x.attachments) != 0:
+
+                if len(x.attachments) >= 1:
                     #I I LOVE YOU I LO3333333333ASOIDJFKS3ADJFMSADJGMSDGOWO<3
                     if len(x.attachments) == 1:
                         embed.set_footer(text = "{}".format(len(x.attachments)) + " attachment below")
                     else:
                         embed.set_footer(text = "{}".format(len(x.attachments)) + " attachments below")
+            
             else:
-                embed.add_field(name = "\u200b", value = x.created_at.strftime("%D @ %T %p") + " - [jump](" + x.jump_url + ")")
-                if len(x.attachments) == 1:
-                    embed.set_footer(text = "{}".format(len(x.attachments)) + " attachment below") # FIX THIS ONE TOO HEH
-                else:
-                    embed.set_footer(text = "{}".format(len(x.attachments)) + " attachments below")
-            await bot.get_channel(defaultChannel).send(embed = embed)
+                #for text/embeds with attachments
+                if len(x.attachments) >= 1:
+                    embed.add_field(name = "\u200b", value = x.created_at.strftime("%D @ %T %p") + " - [jump](" + x.jump_url + ")")
+                    if len(x.attachments) == 1:
+                        embed.set_footer(text = "{}".format(len(x.attachments)) + " attachment below") 
+                    else:
+                        embed.set_footer(text = "{}".format(len(x.attachments)) + " attachments below")
+                    await bot.get_channel(defaultChannel).send(embed = embed) 
+        
+                #for text-only embeds (because these always go into this overarching else statement)
+                if len(x.embeds) != 0:
+                    embed.set_author(name = x.embeds[0].author.name, icon_url = x.embeds[0].author.icon_url)
+                    for y in x.embeds[0].fields:
+                        embed.add_field(name = y.name, value = y.value) #issue: value (date/jump) when there are multiple fields 
+                    await bot.get_channel(defaultChannel).send(embed = embed)
 
-            if len(x.attachments) != 0:
+            if len(x.attachments) >= 1:
                 for y in x.attachments:
                     async with aiohttp.ClientSession() as session:
                         async with session.get(y.url) as resp:
@@ -66,11 +78,20 @@ async def migrate(ctx):
         await ctx.send("Messages have been migrated, silly! uwu")
     else: await ctx.send("There aren't any pins here, silly! òwó")
 
-
+#Random pin generator owo
+@bot.command()
+async def randompin(ctx):
+    messages = await ctx.message.channel.history().flatten()
+    message = messages[random.randint(0,len(messages))]
+    await ctx.send("heres a random pin, silly! ◕w◕ :point_down:")
+    if(len(message.embeds)!=0):
+        await bot.get_channel(defaultChannel).send(embed = message.embeds[0]) #issue: sending two messages (embed AND attachment)
+    else: 
+        await ctx.send("{}".format(message.clean_content))
 
 #literally just to make sure it works
 @bot.command()
 async def test(ctx):
     await ctx.send("yo i work, silly! OwO")
 
-bot.run('NTkyODQ3OTg5OTkxNTM4Njg4.XRaTeg.WzPqNRd_CP2etvU0GNQyX0KJ-yc')
+bot.run('TOKEN')
